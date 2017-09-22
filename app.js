a497613fd62c0aab201ab61ffdd390423ea25b74
JuanMiguel94 <jarce@pernixlabs.com>
2017-09-22 11:08:25 -0600

    Added input validation

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ angular.module("App",['main']);
 
 angular.module("main",[]);
 
- angular.module("main").controller("mainController",function($scope){
-    // Controller body
+angular.module("main").controller("mainController",function($scope){
+    $scope.pattern = new RegExp("£?[0-9]+.?[0-9]*p?");
+    
 });
